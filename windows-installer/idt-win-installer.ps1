b035dee88164f60363f23776993c94f6527de8e2
BJClarke <golfboy1@vt.edu>
2017-12-13 13:44:51 -0600

    Switched order of check
    The check for 64-bit uses language that assumes the OS is Windows 10

--- windows-installer/idt-win-installer.ps1
+++ windows-installer/idt-win-installer.ps1
@@ -2,17 +2,17 @@
 # Installs IBM Developer Bluemix CLI plugin and all dependencies.
 # VERSION="0.8"
 
-# Check for 64-bit Platform - Dev and Helm do not have 32-bit versions.
-if ([Environment]::Is64BitProcess -ne [Environment]::Is64BitOperatingSystem)
-{
-    echo "This installer requires 64-bit Windows."
-    exit
-}
-
 # Check for Windows 10
 if ([System.Environment]::OSVersion.Version.Major -ne 10)
 {
     echo "This installer requires Windows 10."
+    exit
+}
+
+# Check for 64-bit Platform - Dev and Helm do not have 32-bit versions.
+if ([Environment]::Is64BitProcess -ne [Environment]::Is64BitOperatingSystem)
+{
+    echo "This installer requires 64-bit Windows."
     exit
 }
 
