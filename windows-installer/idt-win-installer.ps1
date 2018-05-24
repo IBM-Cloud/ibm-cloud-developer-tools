803ca789ba0dd4e9ec8bea261d4142a1a7dbf3b8
Andrew Trice <amtrice@us.ibm.com@Andrews-MacBook-Pro.local>
2018-05-24 14:56:05 -0400

    updating security protocols for web requests

--- windows-installer/idt-win-installer.ps1
+++ windows-installer/idt-win-installer.ps1
@@ -151,6 +151,7 @@ function install_deps() {
   if( -not (get-command git -erroraction 'silentlycontinue') -or $Global:FORCE) {
     log "Installing/updating external dependency: git"
     $gitVersion = (Invoke-WebRequest "https://git-scm.com/downloads/latest" -UseBasicParsing).Content
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
     Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v$gitVersion.windows.1/Git-$gitVersion-64-bit.exe" -UseBasicParsing -outfile "git-installer.exe"
     .\git-installer.exe /SILENT /PathOption="Cmd" | Out-Null
     Remove-Item "git-installer.exe"
